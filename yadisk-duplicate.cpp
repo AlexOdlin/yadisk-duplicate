808e7e78e5a2e64707b8c73441b4d001006f990e
rsmirnov51 <roman-smr@mail.ru>
2017-06-01 21:12:50 +0300

    Create yadisk-duplicate.cpp

---
+++ yadisk-duplicate.cpp
@@ -0,0 +1,16 @@
+using yadisk::ops::Resource;
+using yadisk::ops::Predicate;
+using yadisk::ops::Tree;
+using yadisk::Client;
+using std::string;
+using std::map;
+ 
+using Resources = std::vector<Resource>;
+ 
+namespace yadisk 
+{
+  namespace ops 
+  {
+      map<string, Resources> find_duplicate(Client& client, Predicate pred = Hash);
+  }
+}
