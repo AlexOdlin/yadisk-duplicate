9c9ada4bc10bf6c2a008eff23d009b0ca8416c91
rsmirnov51 <roman-smr@mail.ru>
2017-06-02 14:14:31 +0400

    Update CPackConfig.cmake

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -1,6 +1,6 @@
 include(InstallRequiredSystemLibraries)
 
-set(CPACK_PACKAGE_CONTACT "designerror@yandex.ru")
+set(CPACK_PACKAGE_CONTACT "roman-smr@mail.ru")
 set(CPACK_PACKAGE_VERSION_MAJOR ${WDC_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${WDC_VERSION_MINOR})
 set(CPACK_PACKAGE_VERSION_PATCH ${WDC_VERSION_PATCH})
@@ -16,5 +16,4 @@ set(CPACK_DEBIAN_PACKAGE_NAME "duplicate-pack-dev")
 set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://github.com/yadisk-ops/yadisk-duplicate")
 set(CPACK_DEBIAN_PACKAGE_PREDEPENDS "cmake >= 3.1")
 set(CPACK_DEBIAN_PACKAGE_RELEASE 1)
-set(CPACK_PROJECT_CONFIG_FILE ${CMAKE_CURRENT_SOURCE_DIR}/cmake/Package.cmake.in)
 include(CPack)
